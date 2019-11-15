da9a319e5dd41853a6889672e0a2a93bdc423b76
perpeer <ekremtaskiran@Ekrems-MacBook-Pro.local>
2019-11-15 12:23:28 +0300

    Xcode project created

---
+++ FlickrService/FlickrService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrService
+//
+//  Created by Ekrem TAŞKIRAN on 15.11.2019.
+//  Copyright © 2019 Ekrem TAŞKIRAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
